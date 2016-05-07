46cf66773a0abf050b26b8d7a418378daa4bf602
impul <pavlo.bojkoo@gmail.com>
2016-05-07 15:10:14 +0300

    Pull

--- file.asm
+++ file.asm
@@ -3,32 +3,31 @@
 	FileName db "Database",0
 	FileHandleTMP DW ?
 	FileNameTMP db "Database_tmp",0		
+
 .code
 ;===============================
 ;Відкриття файла
 ;===============================
 OpenFiles PROC 
-	lea SI, FileName
 	mov BX, 2			
 	mov CX, 0		
-	mov DX, 1	
+	mov DX, offset FileName	
 	 mov AX, 716Ch	 	 
 	 INT 21h
 	jc createFile
 mov FileHandle, AX
 jmp createTMPfile
 createFile:
-	lea SI, FileName
 	mov BX, 2		
 	mov CX, 0		
-	mov DX, 10h		
-	 mov AX, 716Ch	 	 
+	mov DX, offset FileName		
+	 mov AX, 3Ch 	 	 
 	 INT 21h
 	mov FileHandle, AX
 	
 createTMPfile:
 	lea DX, FileNameTMP
-	 mov AH,41h 
+	 mov AH, 41h
 	 INT 21h
 
 	lea SI, FileNameTMP
